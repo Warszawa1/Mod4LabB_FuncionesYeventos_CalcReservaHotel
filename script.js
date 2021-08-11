d06ed80b9202757d3eb871f586e64ef92a1261a3
Warszawa1 <48474962+Warszawa1@users.noreply.github.com>
2021-08-11 10:22:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function calcula() {
+    let numero = document.fdatos.entrada.value;
+    let dato = document.getElementById("lista");
+    let valor = dato.options[lista.selectedIndex].value;
+    let datoP = document.getElementById("parking");
+    let park = datoP.options[parking.selectedIndex].value;
+    
+    //SIGO INTENTANDO QUE LA PARTE DEL CHECKBOX FUNCIONE
+    //let spa = document.fdatos.spasi.value;
+    //let spano = document.fdatos.spano.value;
+    //let spanoN = parseInt(spano);
+    //let spaN = parseInt(spa);
+
+    if (numero === "") {
+        alert("Por favor introduce el número de noches");
+        return;
+    }
+  
+    let hab = (numero * valor);
+    let aparc =(numero * park);
+    let resultado = (hab) + (aparc);
+    
+    document.getElementById("salida").innerHTML = "El total de tu reserva es de:  " + resultado + "€";
+  }
