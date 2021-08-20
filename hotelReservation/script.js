184344c40bdad1513ab0e78cb7a5bc9957cd7ad2
Warszawa1 <48474962+Warszawa1@users.noreply.github.com>
2021-08-20 10:20:12 +0200

    Update script.js

--- hotelReservation/script.js
+++ hotelReservation/script.js
@@ -1,22 +1,38 @@
 function calcula() {
     let numero = document.fdatos.entrada.value;
+
+   
+
     let dato = document.getElementById("lista");
     let valor = dato.options[lista.selectedIndex].value;
     let datoP = document.getElementById("parking");
     let park = datoP.options[parking.selectedIndex].value;
-    //let spa = document.fdatos.spasi.value;
-    //let spano = document.fdatos.spano.value;
-    //let spanoN = parseInt(spano);
-    //let spaN = parseInt(spa);
 
-    if (numero === "") {
-        alert("Por favor introduce el número de noches");
-        return;
-    }
-  
+    let yes = document.getElementById("spasi"); 
+    let no = document.getElementById("spano"); 
+    //let spasi = yes.checked;
+    //let spano = no.checked;
     let hab = (numero * valor);
-    let aparc =(numero * park);
-    let resultado = (hab) + (aparc);
-    
-    document.getElementById("salida").innerHTML = "El total de tu reserva es de:  " + resultado + "€";
-  }
+    let aparc =(numero * park); 
+
+
+    //let result = (hab) * (aparc) + (yes);
+
+
+    //function checkCheckbox() {  
+       
+      if (yes.checked == true && no.checked == true){  
+        return document.getElementById("error").innerHTML = "(Por favor, marca solo una opción)";  
+      }  
+      else if (yes.checked == true){  
+        var y = document.getElementById("spasi").value;  
+        return document.getElementById("result").innerHTML = (hab) + (aparc) + Number(y) + "€";   
+      }   
+      else if (no.checked == true){  
+        var n = document.getElementById("spano").value;  
+        return document.getElementById("result").innerHTML = (hab) + (aparc) + Number(n) + "€";  
+      }  
+      else {  
+        return document.getElementById("error").innerHTML = "*Por favor, marca una de las opciones, Sí o No";  
+      }  
+    }  
